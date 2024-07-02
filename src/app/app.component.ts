56ae2d233cd66c50a829877257c62a27e88ce135
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-07-02 12:54:48 -0300

    feat: navegação em rotas com componentes, formulários reativos e cadastro de produto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    RouterLink,
     RouterOutlet,
     CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-
-  //atributos
-  produtos : any[] = []; //estou diezendo que produtos é uma array de objetos 
-
-  //declarando um objeto HttpClient
-  constructor(private httpClient: HttpClient) {}
-
-
-  ngOnInit(): void {    
-   
-    //fazendo uma requisição para o serviço de consulta de produtos
-    this.httpClient.get('http://localhost:8081/api/produtos')
-      .subscribe({ //aguardando a resposta
-        next: (data) => { //capturando o retorno de sucesso da API
-          //armazenando os dados dos produtos obtidos na API
-          this.produtos = data as any[];
-        },
-        error: (e) => { //capturando o retorno de erro da API
-          console.log(e.error);
-        }
-      })
-  }
+export class AppComponent {
+  
 }
 
 
---
+++ src/app/components/consulta-produtos/consulta-produtos.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-produtos',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './consulta-produtos.component.html',
+  styleUrl: './consulta-produtos.component.css'
+})
+export class ConsultaProdutosComponent implements OnInit {
+
+
+  //atributos
+  produtos: any[] = []; //estou dizendo que produtos é uma array de objetos 
+
+  //declarando um objeto HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+
+  ngOnInit(): void {
+
+    //fazendo uma requisição para o serviço de consulta de produtos
+    this.httpClient.get('http://localhost:8081/api/produtos')
+      .subscribe({ //aguardando a resposta
+        next: (data) => { //capturando o retorno de sucesso da API
+          //armazenando os dados dos produtos obtidos na API
+          this.produtos = data as any[];
+        },
+        error: (e) => { //capturando o retorno de erro da API
+          console.log(e.error);
+        }
+      })
+  }
+}
